899a5b69799a3e1e628d4934032eba509c2ee108
lhwy2 <dlftod87!1>
2023-04-02 17:21:10 +0900

    테스트

---
+++ src/views/DataBindingList.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+  <table>
+    <thead>
+      <tr>
+        <th>제품명</th>
+        <th>가격</th>
+        <th>카테고리</th>
+        <th>배송료</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr :key="i" v-for="(product, i) in productList">
+        <td>{{product.product_name}}</td>
+        <td>{{product.price}}</td>
+        <td>{{product.category}}</td>
+        <td>{{product.delivery_price}}</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</template>
+
+<script>
+export default {
+  name: "DataBindingList",
+  data() {
+    return {
+      productList: []
+    }
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    async getList() {
+      this.productList = await this.$api('https://80a5bff3-1623-4089-8ff7-ebbc37aec025.mock.pstmn.io/list', 'get')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
